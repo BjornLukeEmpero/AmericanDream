98e13fd0273a2e712d27342e858457ad39a732c5
BjornLukeEmpero <43495458+BjornLukeEmpero@users.noreply.github.com>
2023-09-11 21:58:22 +0900

    Update 20230911 Patch_1 / 0.10.4
    Add field items
    Remove useless objects
    Modify Some scripts

--- Assets/Scripts/Entity/Ore.cs
+++ Assets/Scripts/Entity/Ore.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.Antlr3.Runtime;
-using UnityEditor.Purchasing;
 using UnityEngine;
 
 public class Ore : MonoBehaviour
